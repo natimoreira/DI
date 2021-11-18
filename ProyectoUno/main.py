7fc7212c53660b5340c52805e4d975a60fbc1786
Natalia Moreira <51803895+natimoreira@users.noreply.github.com>
2021-11-18 20:21:39 +0100

    ventana gráfica

--- ProyectoUno/main.py
+++ ProyectoUno/main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+from ventana import *
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Main(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super(Main, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QtWidgets.QApplication([])
+    window = Main()
+    window.show()
+    sys.exit(app.exec())
