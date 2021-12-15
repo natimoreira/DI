b156c28c9b455f753462a8af9b673edd8470cbb5
Natalia Moreira <51803895+natimoreira@users.noreply.github.com>
2021-12-15 20:59:10 +0100

    ejercicio 3

--- Proyecto2/events.py
+++ Proyecto2/events.py
@@ -1,8 +1,18 @@
-import sys
+import sys, var
 
 class Eventos:
     def Salir():
         try:
             sys.exit()
+        except Exception as error:
+            print("Error %s: " % str(error))
+
+    def SalirModal(self):
+        try:
+            var.dlgsalir.show()
+            if var.dlgsalir.exec():
+                sys.exit()
+            else:
+                var.dlgsalir.hide()
         except Exception as error:
             print("Error %s: " % str(error))
--- Proyecto2/main.py
+++ Proyecto2/main.py
@@ -1,3 +1,4 @@
+from windowaviso import *
 from ventana import *
 import sys, var, events, clientes
 
@@ -13,8 +14,20 @@ class Main(QtWidgets.QMainWindow):
         '''
         var.ui.lineDNI.editingFinished.connect(clientes.Clientes.validarDNI)
 
+class DialogSalir(QtWidgets.QDialog):
+    def __init__(self):
+        '''
+        Clase que instancia la ventana de aviso salir
+        '''
+        super(DialogSalir, self).__init__()
+        var.dlgsalir = Ui_Dialog()
+        var.dlgsalir.setupUi(self)
+        '''var.dlgsalir.btnBox.accepted.connect(self.accept)
+        var.dlgsalir.btnBox.accepted.rejected(self.reject)'''
+
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
     window = Main()
+    var.dlgsalir = DialogSalir()
     window.show()
     sys.exit(app.exec())
