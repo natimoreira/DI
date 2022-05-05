9c39bd4196b6db78d181c3995710da5203afd030
Natalia Moreira <natimoreiram@gmail.com>
2022-05-05 20:30:00 +0200

    Interfaz agenda

---
+++ ProyectoAgenda/eventos.py
@@ -0,0 +1,22 @@
+import sys, var
+from PyQt5 import QtWidgets
+
+class eventos:
+
+    '''Función para salir'''
+    def Salir():
+        try:
+            sys.exit()
+        except Exception as error:
+            print("Error %s: " % str(error))
+
+    '''Función para salir con el icono'''
+    def SalirModal(self):
+        try:
+            var.dlgsalir.show()
+            if var.dlgsalir.exec():
+                sys.exit()
+            else:
+                var.dlgsalir.hide()
+        except Exception as error:
+            print("Error %s: " % str(error))
--- ProyectoAgenda/main.py
+++ ProyectoAgenda/main.py
@@ -1,4 +1,16 @@
+from ventanaAgenda import *
+import sys, var
+
+class Main(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super(Main, self).__init__()
+        var.ui = Ui_MainWindow()
+        var.ui.setupUi(self)
+
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    app = QtWidgets.QApplication([])
+    window = Main()
+    window.show()
+    sys.exit(app.exec())
